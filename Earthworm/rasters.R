a18e01f5273744df0628dc28f4c44c587a299513
Maximilian Pichler <Maximilian.Pichler@stud.uni-regensburg.de>
2019-11-27 09:42:29 +0100

    z

--- Earthworm/rasters.R
+++ Earthworm/rasters.R
@@ -39,11 +39,13 @@ url <- c(
 Rasters <- data.frame(name = predname, url, path = NA, projection = NA, stringsAsFactors = F)
 
 
+
+
+.gg = sapply(1:length(Rasters$url[!is.na(url)]), 
+             function(u) download.file(Rasters$url[!is.na(url)][u], destfile = Rasters$path[!is.na(url)][u], method = "wget"))
+
 Rasters$path[!is.na(url)] <- paste('Earthworm/rasters', paste0(predname, '.tif'), sep = '/')[!is.na(url)]
 Rasters$projection[!is.na(Rasters$path)] <- sapply(Rasters$path[!is.na(Rasters$path)], function(r) projection(raster(r)))
-
-
-download.file(Rasters$url, destfile = Rasters$path)
 
 
 # Stack rasters -----------------------------------------------------------------
@@ -97,6 +99,56 @@ stackRasters <- function(path,
 R <- na.omit(Rasters)
 predstack <- stackRasters(R$path, R$projection, R$name)
 predstack <- raster::scale(predstack, scale = T)
-
+plot(predstack)
 # plot(predstack)
 
+
+df = predstack@data@values
+df = df[complete.cases(df),]
+head(df)
+
+
+# --------------------
+n <- 100
+x = matrix(runif(6*n, -1, 1), ncol = 6)
+
+library(keras)
+library(dplyr)
+
+data = read.csv("Earthworm/1804_2_sWormModelData.csv")[,-1]
+str(data)
+names(data)
+sites = 
+  data %>% 
+  filter(Study_Name!="birkhofer2013")
+tmp = sites %>% select(logAbundance,logBiomass, SpeciesRichness, elevation, ph_new, 
+                       SLTPPT, CECSOL, ORCDRC)
+imputed = missRanger::missRanger(tmp[,-c(1:3)])
+tmp = cbind(tmp[,1:3], imputed)
+tmp2 = tmp[complete.cases(tmp[,1:3]),]
+sub = mlr::createDummyFeatures(mlr::normalizeFeatures(obj = tmp2[,4:ncol(tmp2)]))
+X = as.matrix(sub)
+Y = tmp2[,1:3]
+
+dnn = keras_model_sequential()
+dnn %>% 
+  layer_dense( input_shape = ncol(X),units = 25L, activation = "relu") %>% 
+  layer_dropout(rate = 0.2) %>% 
+  layer_dense(units = 25L, activation = "relu") %>% 
+    layer_dropout(rate = 0.2) %>% 
+  layer_dense(units = 1L, activation = NULL)
+
+dnn %>% 
+  compile(loss = keras::loss_mean_squared_error, optimizer = keras::optimizer_adamax(lr = 0.01))
+
+hist = 
+  dnn %>% 
+  fit(x = as.matrix(X), y = as.matrix(Y[,3,drop = F]), validation_split = 0.2, epochs = 85L,shuffle = TRUE)
+
+pred_function = function(model, data) {
+  pred = predict(model, as.matrix(data))[,1]
+  return(cbind(p = pred, se = rep(0.01, length(pred))))
+}
+
+ppp = predict(predstack, dnn, fun = pred_function, index = 1)
+plot(ppp, col = topo.colors(10))
