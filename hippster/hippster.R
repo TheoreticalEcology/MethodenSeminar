cdb6137de161f40f119252461f6382910412cda1
Maximilian Pichler <Maximilian.Pichler@stud.uni-regensburg.de>
2019-12-04 10:16:05 +0100

    transfer

--- hippster/hippster.R
+++ hippster/hippster.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(keras)
 
 image = data[91,,,]
-image %>% 
+train_X[1,,,,drop=FALSE] %>% 
   image_to_array() %>%
   `/`(., 255) %>%
   as.raster() %>%
@@ -14,29 +14,77 @@ raw = unlist(lapply(stringr::str_split(labels, "/"), function(i) i[length(i)]))
 
 Y = matrix(as.numeric(substr(raw, 0, 1)), ncol = 1L)
 
+set.seed(42)
+indices = sample.int(130, 30)
+
+
+train_X = data[-indices,,,]
+train_Y = Y[-indices,,drop =FALSE]
+
+test_X = data[indices,,,]
+test_Y = Y[indices,,drop=FALSE]
+
 
 model = keras_model_sequential()
 model %>% 
   layer_conv_2d(input_shape = c(225, 225,3),filters = 16, kernel_size = c(5,5), activation = NULL, use_bias = F) %>% 
   layer_batch_normalization() %>% 
   layer_activation_relu() %>% 
-  layer_dropout(0.2) %>% 
   layer_max_pooling_2d() %>% 
   layer_conv_2d(filters = 16, kernel_size = c(2,2), activation = NULL, use_bias = F) %>% 
   layer_batch_normalization() %>% 
   layer_activation_relu() %>% 
-  layer_dropout(0.2) %>% 
   layer_max_pooling_2d() %>% 
   layer_flatten() %>% 
+  layer_dense(units = 10L) %>% 
+  layer_batch_normalization() %>% 
+  layer_activation_relu() %>% 
   layer_dense(1, activation = "sigmoid")
 summary(model)
 
 model %>% 
   compile(
-    optimizer = keras::optimizer_adam(),
+    optimizer = keras::optimizer_adam(lr = 0.001),
     loss = keras::loss_binary_crossentropy,
     metrics = "accuracy"
   )
 
 model %>% 
-  fit(x = data, y = Y, shuffle = TRUE, batch_size = 10L, validation_split = 0.2)
+  fit(x = train_X, y = train_Y, 
+      shuffle = TRUE, batch_size = 25L, 
+      validation_data = list(test_X, test_Y),
+      epochs = 5L)
+
+
+
+
+#### Transfer learning
+densenet = application_densenet(include_top = FALSE, input_shape  = c(225L, 225L, 3L))
+
+
+model = keras::keras_model(inputs = densenet$input, outputs = 
+                             layer_dense(layer_flatten(densenet$output), units = 1L, activation = "sigmoid")
+)
+keras::freeze_weights(model, from = 1, to = length(model$layers)-1L)
+
+train_X[1,,,] %>% 
+  image_to_array() %>%
+  `/`(., 255) %>%
+  as.raster() %>%
+  plot()
+
+model %>%  predict(train_X[1,,,,drop=FALSE])
+
+model %>% 
+  compile(loss = loss_binary_crossentropy, optimizer = optimizer_adam())
+
+model %>% 
+  fit(
+    x = train_X, 
+    y = train_Y,
+    epochs = 1L,
+    batch_size = 20L,
+    shuffle = T,
+    validation_data = list(test_X, test_Y),
+  )
+
