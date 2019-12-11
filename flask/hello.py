fa371e9549798b45e738713d67dd95cb6fda70d6
Maximilian Pichler <Maximilian.Pichler@stud.uni-regensburg.de>
2019-12-11 10:24:40 +0100

    flask

---
+++ flask/hello.py
@@ -0,0 +1,26 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/",  methods=['GET', 'POST'])
+def index():
+  
+  
+
+    return "Hello World Wide Web!"
+
+
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
